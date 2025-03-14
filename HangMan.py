e9c8ae985a3f6ef99c2132976b3367058ae4ac38
ammanpro <shaikammarpro@gmail.com>
2025-03-14 17:07:46 +0530

    HangMan mini project

---
+++ HangMan.py
@@ -0,0 +1,107 @@
+import random
+stages = [r'''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', r'''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', r'''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+word_list = ["aardvark", "baboon", "camel"]
+
+#  variable 'lives' to keep track of the number of lives left.
+#  Set 'lives' to equal 6.
+
+lives = 6
+chosen_word = random.choice(word_list)
+print(chosen_word)
+
+placeholder = ""
+word_length = len(chosen_word)
+for position in range(word_length):
+    placeholder += "_"
+print(placeholder)
+
+game_over = False
+correct_letters = []
+
+while not game_over:
+    guess = input("Guess a letter: ").lower()
+
+    display = ""
+
+    for letter in chosen_word:
+        if letter == guess:
+            display += letter
+            correct_letters.append(guess)
+        elif letter in correct_letters:
+            display += letter
+        else:
+            display += "_"
+
+    print(display)
+    print(stages[lives])
+
+    
+
+    if "_" not in display:
+        game_over = True
+        print("You win.")
+    elif guess not in chosen_word:
+        lives -= 1
+        print(f"you left with {lives}")
+
+        if lives == 0:
+            print("You lose." )
+            exit()
+
+    
+        print(stages[lives])
---
+++ Hello.py
@@ -0,0 +1,32 @@
+print ("welcome to 'Hyderabad Biryani House' delivery")
+
+quant = (str(input("please type your wished quantity mini or full or family : ")))
+
+total = 0
+
+if quant == "mini" :
+    total += 150
+if quant == "full" : 
+    total += 250
+if quant == "family" :
+    total += 600
+else:
+    print("Invalid quantity! Please choose mini, full, or family.")
+    exit()    
+
+soft_drint = (str(input("if you want to add soft drink press ' y ' : ")))
+if soft_drint == "y" :
+    total += 20 
+
+mayoo = (str(input("if you want to add mayoo press ' y ' : ")))
+if mayoo == "y" :
+    if quant == "family" :
+        total += 60
+    else :
+        total += 30    
+
+pudina_chetuny = (str(input("if you want to add pudina chetuny press ' y ' : ")))
+if soft_drint == "y" :
+    total += 25 
+
+print(f"your total will be : {total}")
